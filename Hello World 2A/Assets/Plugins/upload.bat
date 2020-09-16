31d68571c6d8527b95d48bc1dcc233b00a821c56
TomPiccolo <50777275+TomPiccolo@users.noreply.github.com>
2020-09-16 19:30:43 -0400

    Openedproject

--- Hello World 2A/Assets/Plugins/upload.bat
+++
@@ -1,1 +0,0 @@
-curl -v -b cookie -X PUT -F "space_template[zip]=@/Users/thomaspiccolo/Documents/GitHub/OceanWorld2/Hello World 2A/template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1550391274433938006.json
