22860af4cd84fb15d3c2b0fe7f8240a220440d74
TomPiccolo <50777275+TomPiccolo@users.noreply.github.com>
2020-09-16 19:34:13 -0400

    SecondTryto Revert

--- Hello World 2A/Assets/Prefabs/Plugins/upload.bat
+++
@@ -1,1 +0,0 @@
-curl -v -b cookie -X PUT -F "space_template[zip]=@/Users/thomaspiccolo/Documents/GitHub/OceanWorld2/Hello World 2A/template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1562428769048724135.json
